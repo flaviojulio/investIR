371a0af4673be6104a9dbc2ee89f1431ee1cb074
flaviojulio <flaviojulioa@gmail.com>
2025-06-08 11:55:05 -0300

    povoar tabela dividendos

--- backend/import_acoes_csv.py
+++ backend/import_acoes_csv.py
@@ -2,31 +2,35 @@ import csv
 import sqlite3
 
 DATABASE_FILE = r"C:\Projeto Fortuna\investIR\backend\acoes_ir.db"
-CSV_FILE = 'Acoes.csv'
+CSV_FILE = 'C:\Projeto Fortuna\investIR\backend\Dividendos.csv'
 
 def insert_acoes_from_csv(csv_file):
     conn = sqlite3.connect(DATABASE_FILE)
     cur = conn.cursor()
 
-    with open(csv_file, newline='', encoding='utf-8') as f:
-        reader = csv.DictReader(f)
+    # Abrindo o CSV e inserindo os dados
+    with open(csv_file, newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
         for row in reader:
-            cur.execute("""
-                INSERT OR IGNORE INTO acoes (
-                    id, ticker, nome, razao_social, cnpj, ri,
-                    classificacao, isin
-                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-            """, (
-                row['ID'],
-                row['Ticker'],
-                row['Nome'],
-                row['Razao Social'],
-                row['CNPJ'],
-                row['Site do RI'],
-                row['Classificacao Setorial B3'],
-                row['Codigo ISIN']
+            cur.execute('''
+                INSERT INTO proventos (id_acao, tipo, valor, data_registro,data_ex,dt_pagamento)
+                VALUES (?, ?, ?, ?, ?, ?)
+            ''', (
+                row['ID_Acao'],
+                row['Tipo'],
+                row['Valor'],
+                row['data_registro'],
+                row['data_ex'],
+                row['dt_pagamento']
             ))
+
+ID_Acao, Tipo, Valor, data_registro,data_ex,dt_pagamento
+
+    # Salvando as alterações e fechando a conexão
     conn.commit()
+    conn.close()
+
+    print("Dados inseridos com sucesso!")    
     cur.close()
     conn.close()
     print("Importação concluída.")
